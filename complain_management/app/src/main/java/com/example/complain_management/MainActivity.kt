b36ec10c67b1f095c7c1dd809980210467680cfa
shikhar Gupta <shikhargupta442003@gmail.com>
2023-12-14 11:05:23 +0530

    firebase connected

---
+++ complain_management/app/src/main/java/com/example/complain_management/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.complain_management
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.complain_management.databinding.ActivityMainBinding
+import com.example.complain_management.databinding.ActivitySignInBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonSignin.setOnClickListener{
+            val intent= Intent(this,SignInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
