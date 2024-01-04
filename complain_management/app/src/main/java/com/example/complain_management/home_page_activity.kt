e6b862d277e3220195ae30a0e0ef4e3cc100331a
shikhar Gupta <shikhargupta442003@gmail.com>
2024-01-04 21:25:43 +0530

    home_page_ui-complete

---
+++ complain_management/app/src/main/java/com/example/complain_management/home_page_activity.kt
@@ -0,0 +1,11 @@
+package com.example.complain_management
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home_page_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_page)
+    }
+}
